a96a9a2d4849b777b6bec04a61319c829db8e940
RickySkywalker <rwangbr@connect.ust.hk>
2024-06-14 12:23:32 +0800

    修改了长文本评测框架

--- evaluate.sh
+++ evaluate.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-model_name_or_path="/home/nlpintern1/liyu/models/Llama-7b-hf"
-CUDA_VISIBLE_DEVICES=4,5
-deepspeed --include localhost:${CUDA_VISIBLE_DEVICES} examples/evaluation.py \
---arch_type decoder_only \
+model_name_or_path="/home/nlpintern1/liyu/models/TinyLlama-1.1B-Chat-v1.0"
+CUDA_VISIBLE_DEVICES=2,4,5
+deepspeed --include localhost:${CUDA_VISIBLE_DEVICES} examples/evaluation_gpu.py \
+--cache_dir ./.cache/ \
+--arch_type topk \
 --answer_type text \
 --model_name_or_path ${model_name_or_path} \
 --dataset_path data/wiki_en_eval \
@@ -10,3 +11,16 @@ deepspeed --include localhost:${CUDA_VISIBLE_DEVICES} examples/evaluation.py \
 --inference_batch_size_per_device 1 \
 --block_size 4096 \
 --metric ppl
+
+# # cpu
+# python examples/evaluation_cpu.py \
+# --cache_dir ./.cache/ \
+# --arch_type topk \
+# --answer_type text \
+# --model_name_or_path ${model_name_or_path} \
+# --dataset_path data/wiki_en_eval \
+# --deepspeed examples/ds_config.json \
+# --inference_batch_size_per_device 1 \
+# --block_size 4096 \
+# --use_accelerator_for_evaluator True \
+# --metric ppl
